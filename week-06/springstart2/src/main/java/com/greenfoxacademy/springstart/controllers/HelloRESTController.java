618fd586b1691bce75efdb0f36c82396983404b2
Attila Fekete <feketeattila89@gmail.com>
2019-05-15 15:32:20 +0200

    Sayhellototheworld works fine, but does not change color. Need to utilize the html file and css for the solution.

---
+++ week-06/springstart2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloRESTController {
+  AtomicLong myAtom = new AtomicLong();
+
+  @RequestMapping ("/greeting")
+  public String greeting (@RequestParam String name) {
+    Greeting greeting = new Greeting(myAtom.incrementAndGet(), name);
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    String answer = hellos[(int) (Math.random() * hellos.length + 1)] + " " + greeting.getContent();
+   return answer;
+  }
+}
