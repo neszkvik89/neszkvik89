62725465cca00de6477c7f97adf3534c55b3ce45
Attila Fekete <feketeattila89@gmail.com>
2019-05-31 10:29:06 +0200

    Upvote-downvote links work as intended

--- week-08/day05/src/main/java/com/example/demo/controller/PostController.java
+++ week-08/day05/src/main/java/com/example/demo/controller/PostController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Post;
 import com.example.demo.repository.IPostRepository;
 import com.example.demo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PostController {
@@ -20,6 +22,24 @@ public class PostController {
 
   @GetMapping("/")
   public String index (Model model) {
+    model.addAttribute("posts", iPostRepository.findAll());
+    return "main";
+  }
+
+  @GetMapping("/upvote/{id}")
+  public String upvote(@PathVariable ("id") long id, Model model) {
+    Post myPost = iPostRepository.findById(id).orElseGet(null);
+    myPost.setUpvotes(myPost.getUpvotes() + 1);
+    iPostRepository.save(myPost);
+    model.addAttribute("posts", iPostRepository.findAll());
+    return "main";
+  }
+
+  @GetMapping("/downvote/{id}")
+  public String downvote(@PathVariable ("id") long id, Model model) {
+    Post myPost = iPostRepository.findById(id).orElseGet(null);
+    myPost.setUpvotes(myPost.getUpvotes() - 1);
+    iPostRepository.save(myPost);
     model.addAttribute("posts", iPostRepository.findAll());
     return "main";
   }
