61e73ef8698b63225cbeb20608ccdc8ee0a6c030
Attila Fekete <feketeattila89@gmail.com>
2019-06-20 12:13:15 +0200

    Token handler now uses environment variable SECRET_KEY instead of local private final String

--- movie/src/main/java/com/example/movie/JWTHandler.java
+++ movie/src/main/java/com/example/movie/JWTHandler.java
@@ -9,27 +9,21 @@ import java.util.Date;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
-public class JWTDemo {
+public class JWTHandler {
 
   private static final String SECRET_KEY = "k7jfjsdja89fksalmcfiusah9jfslamf9sdjfasldfn389fj2udshjfansdu"
       + "sdf89asjdf9asbdu9nfsa9ufnsad9f8uhaasnd9funbds9ufpn9ufabd9uffah";
 
   public static String createJWT(String id, String issuer, String subject) {
 
-
-
-
-    //The JWT signature algorithm we will be using to sign the token
     SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     long nowMillis = System.currentTimeMillis();
     Date now = new Date(nowMillis);
 
-    //We will sign our JWT with our ApiKey secret
-    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(System.getenv("SECRET_KEY"));
     Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
-    //Let's set the JWT Claims
     JwtBuilder builder = Jwts.builder().setId(id)
         .setIssuedAt(now)
         .setSubject(subject)
@@ -37,14 +31,12 @@ public class JWTDemo {
         .setExpiration(new Date(System.currentTimeMillis() + (1000*30)))
         .signWith(signatureAlgorithm, signingKey);
 
-    //Builds the JWT and serializes it to a compact, URL-safe string
     return builder.compact();
   }
 
   public static Claims decodeJWT(String jwt) {
-    //This line will throw an exception if it is not a signed JWS (as expected)
     return Jwts.parser()
-        .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+        .setSigningKey(DatatypeConverter.parseBase64Binary(System.getenv("SECRET_KEY")))
         .parseClaimsJws(jwt).getBody();
   }
 
